fd3ea9cf4120362368c4cdfc418878fe71ab2f1c
Markku Halinen <welociraptor@gmail.com>
2023-12-01 07:52:14 +0000

    Start working on day 1 solution

--- day1/index.ts
+++ day1/index.ts
@@ -1,5 +1,8 @@
 import fs from "node:fs/promises";
 
-console.log(await fs.readFile("day1/testdata", { encoding: "utf-8" }));
+const testdata = await fs.readFile("day1/testdata", { encoding: "utf-8" });
 
-console.log("day 1 solution comes here.");
+for (const input of testdata.split(/\n/)) {
+  const matches = Array.from(input.matchAll(/(\d)/g));
+  for (const match of matches) console.log(match[0]);
+}
